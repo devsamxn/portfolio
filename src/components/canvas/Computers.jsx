ce19ff4f30372c1897be4170c44f4902d0546834
Sameer <negisameer1926@gmail.com>
2024-08-27 12:43:00 +0530

    check for deployment

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("/public/desktop_pc/scene.gltf");
+  const computer = useGLTF("/desktop_pc/scene.gltf");
   return (
     <mesh>
       <ambientLight intensity={0.5} />
